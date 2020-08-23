0aca26f8ca2c5b45e07d9c18f3a8ecfcaa85855b
Lakshya Sukhralia <lakshyasukhralia97@gmail.com>
2020-08-23 21:53:08 +0530

    changes

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: build/install/kotlinserver/bin/kotlinserver
+web:    java -jar build/libs/appport-academy.book-store.jar
--- scripts/book_curl.bat
+++ scripts/book_curl.bat
@@ -4,3 +4,4 @@ curl -X PUT -d '{"id":"10","title":"Avengers","author":"Stan Lee","price":850.0}
 curl -X POST -d '{"id":"10","title":"Avengers","author":"Stan Lee","price":850.0}' -H "Content-Type: application/json" localhost:8080/book/
 curl "localhost:8080/book/list?sortBy=id&asc=false"
 curl -u "shopper1:password" "localhost:8080/book/list?sortBy=id&asc=false"
+mongo "mongodb+srv://cluster0.nyawk.mongodb.net/book_store" --username lakshya
